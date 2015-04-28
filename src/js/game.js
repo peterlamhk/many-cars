6a84c99268c4b956099e4a45bcc011a90717fe4c
Mok Ka Chun <mkc188@users.noreply.github.com>
2015-04-28 15:10:38 +0800

    Add a turning car

--- src/js/game.js
+++ src/js/game.js
@@ -11,9 +11,14 @@
       var x = this.game.width / 2
         , y = this.game.height / 2;
 
-      this.player = this.add.sprite(x, y, 'player');
-      this.player.anchor.setTo(0.5, 0.5);
-      this.input.onDown.add(this.onInputDown, this);
+      // this.player = this.add.sprite(x, y, 'player');
+      // this.player.anchor.setTo(0.5, 0.5);
+      // this.input.onDown.add(this.onInputDown, this);
+
+      this.car = this.add.sprite(x, y, 'cars');
+      this.car.anchor.setTo(0.5, 0.5);
+      this.car.animations.add('turn', Phaser.Animation.generateFrameNames('car', 0, 23, '', 2), 5, true);
+      this.car.animations.play('turn');
     },
 
     update: function () {
@@ -25,14 +30,14 @@
       cy = this.world.centerY;
 
       angle = Math.atan2(y - cy, x - cx) * (180 / Math.PI);
-      this.player.angle = angle;
+      // this.player.angle = angle;
 
       dx = x - cx;
       dy = y - cy;
       scale = Math.sqrt(dx * dx + dy * dy) / 100;
 
-      this.player.scale.x = scale * 0.6;
-      this.player.scale.y = scale * 0.6;
+      // this.player.scale.x = scale * 0.6;
+      // this.player.scale.y = scale * 0.6;
     },
 
     onInputDown: function () {
--- src/js/preloader.js
+++ src/js/preloader.js
@@ -13,13 +13,14 @@
 
       this.load.onLoadComplete.addOnce(this.onLoadComplete, this);
       this.load.setPreloadSprite(this.asset);
-      
+
       this.loadResources();
     },
-      
+
     loadResources: function () {
       this.load.image('player', 'assets/player.png');
       this.load.bitmapFont('minecraftia', 'assets/minecraftia.png', 'assets/minecraftia.xml');
+      this.load.atlas('cars', 'assets/cars.png', 'assets/cars.json');
     },
 
     create: function () {
