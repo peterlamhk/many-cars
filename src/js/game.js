d14408039c53061d29d93a5366145dbadece5142
Mok Ka Chun <mkc188@users.noreply.github.com>
2015-04-28 18:39:22 +0800

    Add basic car movements

--- src/js/game.js
+++ src/js/game.js
@@ -3,8 +3,10 @@
 
   function Game() {
     this.player = null;
-    this.steeringAngle = 0;
+    this.steeringAngle = 270;
     this.steeringMultiplier = 2;
+    this.currentSpeed = 0;
+    this.backwardSpeed = 0;
   }
 
   Game.prototype = {
@@ -21,7 +23,10 @@
       this.car.anchor.setTo(0.5, 0.5);
       // this.car.animations.add('turn', Phaser.Animation.generateFrameNames('car', 0, 23, '', 2), 5, true);
       // this.car.animations.play('turn');
-      this.car.frame = 23;
+      this.car.frame = 18;
+
+      this.game.physics.enable(this.car, Phaser.Physics.ARCADE);
+      this.car.body.maxVelocity.setTo(400, 400);
 
       this.cursors = this.game.input.keyboard.createCursorKeys();
     },
@@ -50,6 +55,38 @@
       } else if (this.cursors.right.isDown) {
         this.steeringAngle = this.steeringAngle < 359 ? this.steeringAngle + this.steeringMultiplier : 0;
         this.car.frame = Math.floor(this.steeringAngle * 24 / 360);
+      } else if (this.cursors.up.isDown) {
+        if (this.currentSpeed < 150) {
+          this.currentSpeed += 4;
+        }
+        if (this.backwardSpeed < 0) {
+          this.backwardSpeed += 4;
+        }
+      } else if (this.cursors.down.isDown) {
+        if (this.currentSpeed > -100) {
+          this.currentSpeed -= 4;
+        } else if (this.backwardSpeed > -100){
+          this.backwardSpeed -= 4;
+        }
+      }
+
+      if (!this.cursors.up.isDown && !this.cursors.down.isDown){
+        if (this.currentSpeed > 2) {
+          this.currentSpeed -= 2;
+          this.backwardSpeed -= 2;
+        } else if (this.currentSpeed < -2){
+          this.currentSpeed += 2;
+          this.backwardSpeed += 2;
+        } else {
+          this.currentSpeed = 0;
+          this.backwardSpeed = 0;
+        }
+      }
+
+      if (this.currentSpeed >= 0) {
+        this.game.physics.arcade.velocityFromAngle(this.steeringAngle, this.currentSpeed, this.car.body.velocity);
+      } else {
+        this.game.physics.arcade.velocityFromAngle(this.steeringAngle, this.backwardSpeed, this.car.body.velocity);
       }
     },
 
