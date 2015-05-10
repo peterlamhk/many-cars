40d0cd19994f761077758fc89c91b1a4302ff4d8
Mok Ka Chun <mkc188@users.noreply.github.com>
2015-05-10 21:36:18 +0800

    Add track1 images and change game scale

--- src/js/boot.js
+++ src/js/boot.js
@@ -4,7 +4,7 @@
   function Boot() {}
 
   Boot.prototype = {
-    
+
     preload: function () {
       this.load.image('preloader', 'assets/preloader.gif');
     },
@@ -12,18 +12,15 @@
     create: function () {
       this.game.input.maxPointers = 1;
 
-      if (this.game.device.desktop) {
-        this.game.scale.pageAlignHorizontally = true;
-      } else {
-        this.game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-        this.game.scale.minWidth =  640;
-        this.game.scale.minHeight = 480;
-        this.game.scale.maxWidth = 1280;
-        this.game.scale.maxHeight = 1024;
-        this.game.scale.forceOrientation(true);
-        this.game.scale.pageAlignHorizontally = true;
-        this.game.scale.setScreenSize(true);
-      }
+      this.game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+      this.game.scale.minWidth =  640;
+      this.game.scale.minHeight = 480;
+      this.game.scale.maxWidth = 1280;
+      this.game.scale.maxHeight = 720;
+      this.game.scale.forceOrientation(true);
+      this.game.scale.pageAlignHorizontally = true;
+      this.game.scale.setScreenSize(true);
+
       this.game.state.start('preloader');
     }
   };
