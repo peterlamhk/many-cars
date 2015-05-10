14e13e0b68e18fda2a3b94283fba5d659a6e23cb
Mok Ka Chun <mkc188@users.noreply.github.com>
2015-05-10 23:13:49 +0800

    Test drawing circle body on the car

--- src/js/game.js
+++ src/js/game.js
@@ -62,15 +62,15 @@
       this.track1aR.body.loadPolygon('physicsData', 'track1aR');
 
       this.car = this.add.sprite(x, y, 'cars');
-      this.car.cover = this.add.sprite(0, 0, 'cars_cover');
       this.car.anchor.set(0.5);
-      this.car.cover.anchor.set(0.5);
-      this.car.addChild(this.car.cover);
 
-      this.car.frame = this.car.cover.frame = 0;
+      this.car.frame = 0;
 
       // this.game.physics.enable(this.car, Phaser.Physics.ARCADE);
       // this.car.body.maxVelocity.set(400);
+      this.game.physics.p2.enable(this.car, true);
+      this.car.body.setCircle(14);
+      this.car.body.static = true;
 
       this.cursors = this.game.input.keyboard.createCursorKeys();
       this.game.input.onDown.add(this.click, this);
@@ -78,117 +78,117 @@
 
     update: function() {
 
-      // if (this.currentSpeed > 250) {
-      //   this.baseSpeed = 0.25;
-      //   this.steeringMultiplier = this.cursors.up.isDown ? 1 : 2;
-      // } else if (this.currentSpeed > 200) {
-      //   this.baseSpeed = 0.5;
-      //   this.steeringMultiplier = this.cursors.up.isDown ? 1.5 : 2;
-      // } else if (this.currentSpeed > 150) {
-      //   this.baseSpeed = 0.75;
-      //   this.steeringMultiplier = this.cursors.up.isDown ? 1.5 : 2;
-      // } else {
-      //   this.baseSpeed = 1;
-      //   this.steeringMultiplier = 2;
-      // }
+      if (this.currentSpeed > 250) {
+        this.baseSpeed = 0.25;
+        this.steeringMultiplier = this.cursors.up.isDown ? 1 : 2;
+      } else if (this.currentSpeed > 200) {
+        this.baseSpeed = 0.5;
+        this.steeringMultiplier = this.cursors.up.isDown ? 1.5 : 2;
+      } else if (this.currentSpeed > 150) {
+        this.baseSpeed = 0.75;
+        this.steeringMultiplier = this.cursors.up.isDown ? 1.5 : 2;
+      } else {
+        this.baseSpeed = 1;
+        this.steeringMultiplier = 2;
+      }
 
-      // if (this.cursors.left.isDown) {
-      //   if (this.currentSpeed > 0) {
-      //     this.steeringAngle = this.steeringAngle > 0 ? this.steeringAngle - this.steeringMultiplier : 359;
-      //   } else if (this.currentSpeed < 0) {
-      //     this.steeringAngle = this.steeringAngle < 359 ? this.steeringAngle + this.steeringMultiplier : 0;
-      //   }
-      // } else if (this.cursors.right.isDown) {
-      //   if (this.currentSpeed > 0) {
-      //     this.steeringAngle = this.steeringAngle < 359 ? this.steeringAngle + this.steeringMultiplier : 0;
-      //   } else if (this.currentSpeed < 0) {
-      //     this.steeringAngle = this.steeringAngle > 0 ? this.steeringAngle - this.steeringMultiplier : 359;
-      //   }
-      // }
+      if (this.cursors.left.isDown) {
+        if (this.currentSpeed > 0) {
+          this.steeringAngle = this.steeringAngle > 0 ? this.steeringAngle - this.steeringMultiplier : 359;
+        } else if (this.currentSpeed < 0) {
+          this.steeringAngle = this.steeringAngle < 359 ? this.steeringAngle + this.steeringMultiplier : 0;
+        }
+      } else if (this.cursors.right.isDown) {
+        if (this.currentSpeed > 0) {
+          this.steeringAngle = this.steeringAngle < 359 ? this.steeringAngle + this.steeringMultiplier : 0;
+        } else if (this.currentSpeed < 0) {
+          this.steeringAngle = this.steeringAngle > 0 ? this.steeringAngle - this.steeringMultiplier : 359;
+        }
+      }
 
-      // if (this.cursors.left.isDown || this.cursors.right.isDown) {
-      //   var angle = this.steeringAngle-270;
-      //   if (angle < 0){
-      //     angle += 360;
-      //   }
+      if (this.cursors.left.isDown || this.cursors.right.isDown) {
+        var angle = this.steeringAngle-270;
+        if (angle < 0){
+          angle += 360;
+        }
 
-      //   var frame = Math.floor(angle * (119/7*2) / 360) *7;
-      //   if (frame>119){
-      //     this.car.scale.x = -1;
-      //     frame = 119*2 - frame;
+        var frame = Math.floor(angle * (119/7*2) / 360) *7;
+        if (frame>119){
+          this.car.scale.x = -1;
+          frame = 119*2 - frame;
 
-      //   } else {
-      //     this.car.scale.x = 1;
-      //     if (angle > 90 && frame === 119){
-      //       frame=112;
-      //     }
-      //   }
-      //   this.car.frame = this.car.cover.frame = frame;
-      // }
+        } else {
+          this.car.scale.x = 1;
+          if (angle > 90 && frame === 119){
+            frame=112;
+          }
+        }
+        this.car.frame = frame;
+      }
 
-      // if (this.cursors.up.isDown) {
-      //   if (this.currentSpeed < 300) {
-      //     this.currentSpeed += this.baseSpeed;
-      //   }
-      //   if (this.backwardSpeed < 0) {
-      //     this.backwardSpeed += this.baseSpeed;
-      //   }
+      if (this.cursors.up.isDown) {
+        if (this.currentSpeed < 300) {
+          this.currentSpeed += this.baseSpeed;
+        }
+        if (this.backwardSpeed < 0) {
+          this.backwardSpeed += this.baseSpeed;
+        }
 
-      //   if (this.currentSpeed > 250 && this.skiddingSpeed < 100) {
-      //     this.skiddingSpeed += 1;
-      //   }
+        if (this.currentSpeed > 250 && this.skiddingSpeed < 100) {
+          this.skiddingSpeed += 1;
+        }
 
-      //   if (this.drifting) {
-      //     if (this.reduceSpeed) {
-      //       this.currentSpeed *= 0.75;
-      //       this.reduceSpeed = false;
-      //     }
+        if (this.drifting) {
+          if (this.reduceSpeed) {
+            this.currentSpeed *= 0.75;
+            this.reduceSpeed = false;
+          }
 
-      //     if (this.currentSpeed >= 0) {
-      //       this.game.physics.arcade.velocityFromAngle(this.steeringAngle, this.currentSpeed, this.car.body.velocity);
-      //     } else {
-      //       this.game.physics.arcade.velocityFromAngle(this.steeringAngle, this.backwardSpeed, this.car.body.velocity);
-      //     }
-      //   }
-      // } else if (this.cursors.down.isDown) {
-      //   if (this.currentSpeed > -100) {
-      //     this.currentSpeed -= 4;
-      //   } else if (this.backwardSpeed > -100) {
-      //     this.backwardSpeed -= 4;
-      //   }
-      // } else {
-      //   if (this.currentSpeed > 2) {
-      //     this.currentSpeed -= 2;
-      //     this.backwardSpeed -= 2;
-      //   } else if (this.currentSpeed < -2) {
-      //     this.currentSpeed += 2;
-      //     this.backwardSpeed += 2;
-      //   } else {
-      //     this.currentSpeed = 0;
-      //     this.backwardSpeed = 0;
-      //   }
-      // }
+          if (this.currentSpeed >= 0) {
+            // this.game.physics.arcade.velocityFromAngle(this.steeringAngle, this.currentSpeed, this.car.body.velocity);
+          } else {
+            // this.game.physics.arcade.velocityFromAngle(this.steeringAngle, this.backwardSpeed, this.car.body.velocity);
+          }
+        }
+      } else if (this.cursors.down.isDown) {
+        if (this.currentSpeed > -100) {
+          this.currentSpeed -= 4;
+        } else if (this.backwardSpeed > -100) {
+          this.backwardSpeed -= 4;
+        }
+      } else {
+        if (this.currentSpeed > 2) {
+          this.currentSpeed -= 2;
+          this.backwardSpeed -= 2;
+        } else if (this.currentSpeed < -2) {
+          this.currentSpeed += 2;
+          this.backwardSpeed += 2;
+        } else {
+          this.currentSpeed = 0;
+          this.backwardSpeed = 0;
+        }
+      }
 
-      // if (!this.cursors.up.isDown) {
-      //   if (this.skiddingSpeed > 0) {
-      //     this.skiddingSpeed -= 1;
-      //   }
+      if (!this.cursors.up.isDown) {
+        if (this.skiddingSpeed > 0) {
+          this.skiddingSpeed -= 1;
+        }
 
-      //   if (this.drifting) {
-      //     this.reduceSpeed = true;
-      //   }
-      // }
+        if (this.drifting) {
+          this.reduceSpeed = true;
+        }
+      }
 
-      // if (!this.skiddingSpeed) {
-      //   if (this.currentSpeed >= 0) {
-      //     this.game.physics.arcade.velocityFromAngle(this.steeringAngle, this.currentSpeed, this.car.body.velocity);
-      //   } else {
-      //     this.game.physics.arcade.velocityFromAngle(this.steeringAngle, this.backwardSpeed, this.car.body.velocity);
-      //   }
-      //   this.drifting = false;
-      // } else if (!this.drifting) {
-      //   this.drifting = true;
-      // }
+      if (!this.skiddingSpeed) {
+        if (this.currentSpeed >= 0) {
+          // this.game.physics.arcade.velocityFromAngle(this.steeringAngle, this.currentSpeed, this.car.body.velocity);
+        } else {
+          // this.game.physics.arcade.velocityFromAngle(this.steeringAngle, this.backwardSpeed, this.car.body.velocity);
+        }
+        this.drifting = false;
+      } else if (!this.drifting) {
+        this.drifting = true;
+      }
     },
 
     onInputDown: function() {
--- src/js/preloader.js
+++ src/js/preloader.js
@@ -21,7 +21,6 @@
       this.load.image('player', 'assets/player.png');
       this.load.bitmapFont('minecraftia', 'assets/minecraftia.png', 'assets/minecraftia.xml');
       this.load.atlas('cars', 'assets/cars_base.png', 'assets/cars_base.json');
-      this.load.atlas('cars_cover', 'assets/cars_cover_2.png', 'assets/cars_cover_2.json');
       this.load.image('track1aL', 'assets/track1aL.png');
       this.load.image('track1aR', 'assets/track1aR.png');
       this.load.image('track1bL', 'assets/track1bL.png');
